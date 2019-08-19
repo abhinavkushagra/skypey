5b9aaf1706694018a3b1f8638d8ea7cbdeb09f63
abhinavkushagra <abhinavkushagra@gmail.com>
2019-08-19 18:01:55 +0530

    Basic Layout

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Sidebar from "./Sidebar";
+import Main from "./Main";
+import "./App.css"
+
+const App = () => {
+    return (
+        <div className = "App">
+            <Sidebar />
+            <Main />
+        </div>
+    )
+}
+
+export default App;
