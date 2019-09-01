c38aabc02d7f3fd57fe52f59813a9b1250d45a34
abhinavkushagra <abhinavkushagra@gmail.com>
2019-09-02 03:12:10 +0530

    Added till SEND_MESAGE

--- src/components/ChatWindow.js
+++ src/components/ChatWindow.js
@@ -1,9 +1,23 @@
 import React from "react";
+import store from "../store";
+import Header from "../components/Header";
+import _ from "lodash";
+import Chats from "../containers/Chats";
+import "./ChatWindow.css";
+import MessageInput from "../containers/MessageInput"
+
+
 
 const ChatWindow = ({ activeUserId }) => {
+    const state = store.getState();
+    const activeUser = state.contacts[activeUserId];
+    const activeMsgs = state.messages[activeUserId];
+    const { typing } = state;
     return (
-        <div className="ChatWindow">Conversation for user id:
-            {activeUserId}
+        <div className="ChatWindow">
+            <Header user={activeUser} />
+            <Chats messages= {_.values(activeMsgs)} />
+            <MessageInput value={typing} />
         </div>
     );
 };
